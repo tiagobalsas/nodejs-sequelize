afc968ee8f0010f4e84f73d9f8e87827f57fe6cb
Tiago Santos <tiago881@gmail.com>
2023-03-14 19:06:08 -0300

    feat: Create Addresses conection

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,11 @@ const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
 const User = require('../models/User');
+const Addresses = require('../models/Addresses');
 
 const conection = new Sequelize(dbConfig);
 
 User.init(conection);
+Addresses.init(conection);
 
 module.exports = conection;
