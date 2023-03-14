00baaa7086985225199ba0e38ebb20a768583a3e
Tiago Santos <tiago881@gmail.com>
2023-03-14 16:31:03 -0300

    feat: Create User model

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
+const User = require('../models/User');
+
 const conection = new Sequelize(dbConfig);
+
+User.init(conection);
 
 module.exports = conection;
